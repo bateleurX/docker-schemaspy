9721255d9bf98b746d5030bfd18d895cf1630fe6
Andrew Che <andrew.che@annalise.ai>
2022-04-04 16:56:48 +1000

    feat: configure aws cli

--- docker/schemaspy.sh
+++ docker/schemaspy.sh
@@ -7,11 +7,16 @@ ls -Ax $DRIVER_PATH | sed -e 's/  */, /g'
 
 DB_NAMES=${DB_NAMES:?"List of DB names need to be provided, separated by commas."}
 
-IFS=',' read -ra NAMES <<< "$IN"
+echo "Set up AWS ..."
+aws configure set region "${AWS_REGION:-ap-southeast-2}"
+aws configure set output "json"
+
+# shellcheck disable=SC2039
+IFS=',' read -ra NAMES <<< "$DB_NAMES"
 for db in "${NAMES[@]}"; do
-    exec java -jar /usr/local/lib/schemaspy/schemaspy*.jar -dp $DRIVER_PATH -o /output -db "$db" "$@"
+    java -jar /usr/local/lib/schemaspy/schemaspy*.jar -dp $DRIVER_PATH -o /output -db "$db" "$@"
     # Test for now
-    ls /output/${DB_NAMES:-}
+    ls "/output/${db:-}"
 done
 
 # TODO: upload the output to S3
